da719a7b3f0191994664d7a1159b02bb50890d7b
Jonathan Souza <souzajonathan570@gmail.com>
2021-06-21 17:19:20 -0300

    exercise one complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+import React, { Component } from 'react'
+import MyContext from './context/MyContext';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      }
+    }
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(color, value) {
+    this.setState({
+      cars: { [color]: value }
+    });
+  }
+
+  render() {
+    const contextValue = {
+      cars: this.state.cars,
+      moveCar: this.moveCar,
+    };
+    return (
+      <MyContext.Provider value={contextValue}>
+        <Cars />
+      </MyContext.Provider>
+    )
+  }
 }
 
-export default App;
+export default App
+
